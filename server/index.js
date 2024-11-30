3135dcd471cfe882737faa57c95fad7a5829653b
Colin McDermott <colinm0125@gmail.com>
2024-11-29 21:11:42 -0500

    basic hello world test

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/users', userRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
